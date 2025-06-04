0a869d23562e418a10036eceb6244d826dc67d3d
sfsclone <sultan.farid58@gmail.com>
2025-06-04 15:07:32 +0700

    merged

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -5,7 +5,7 @@ public class MainMenuManager : MonoBehaviour
 {
     public void StartNewGame()
     {
-        SceneManager.LoadScene("GameScene"); // Make sure the game scene is added in Build Settings
+        SceneManager.LoadScene("GameScene1"); // Make sure the game scene is added in Build Settings
     }
 
     public void QuitGame()
